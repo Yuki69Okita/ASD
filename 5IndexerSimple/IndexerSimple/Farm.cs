98e23ab9c6f471452105937b3b4e66333b9f5ad1
trendo <trendo@abv.bg>
2020-02-23 10:49:41 +0200

    add project 5 - Indexer

---
+++ 5IndexerSimple/IndexerSimple/Farm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexerSimple
+{
+    /// <summary>
+    /// Farm is a class which can contain up to 10 animals.
+    /// It can be accessed and set with an Indexer for specific position between 0 and 9
+    /// </summary>
+    public class Farm
+    {
+        private string[] animals = new string[10];
+
+        public string this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index <= animals.Length)
+                {
+                    return animals[index];
+                }
+                return "Error";
+            }
+            set
+            {
+                if (index >= 0 && index < animals.Length && !string.IsNullOrEmpty(value))
+                {
+                    animals[index] = value;
+                }
+            }
+        }
+
+        public int GetNumberAnimals()
+        {
+            return animals.Length;
+        }
+    }
+}
---
+++ 5IndexerSimple/IndexerSimple/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexerSimple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Farm myFarm = new Farm() { [1] = "Cat", [3] = "Bird",[5]= "Dog" };
+            myFarm[2] = "Rabbit";
+            myFarm[7] = "turtle";
+
+            myFarm[100] = "pinguin";
+            myFarm[5] = "pinguin";
+
+            for (int i = 0; i < myFarm.GetNumberAnimals(); i++)
+            {
+                Console.WriteLine("Animals[{0}]={1}",i, myFarm[i] ?? "empty");
+            }
+            Console.WriteLine();
+            Console.WriteLine(myFarm[200]);
+            Console.WriteLine();
+        }
+    }
+}
