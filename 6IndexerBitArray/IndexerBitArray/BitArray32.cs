49fbf058a97ef064d71e193a1e73fa265c815be0
trendo <trendo@abv.bg>
2020-02-23 13:36:14 +0200

    added project 6

---
+++ 6IndexerBitArray/IndexerBitArray/BitArray32.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexerBitArray
+{
+
+    struct BitArray32
+    {
+        private uint mValue;
+        public int this[int index]
+        {
+            get
+            {
+                if (index >= 0 && index <= 31)
+                {
+                    if ((mValue & (1 << index)) == 0)
+                        return 0;
+                    else
+                        return 1;
+                }
+                else
+                {
+                    throw new ApplicationException(string.Format("Index {0} is invalid!", index));
+                }
+            }
+            set
+            {
+                if (index < 0 || index > 31)
+                {
+                    throw new ApplicationException(string.Format("Index {0} is invalid.", index));
+                }
+                if (value < 0 || value > 1)
+                {
+                    throw new ApplicationException(String.Format("Value {0} is invalid.", value));
+                }
+                //Clear the bit at position index
+                mValue &= ~((uint)(1 << index));
+
+                //Set the bit at position index to value
+                mValue |= (uint)(value << index);
+            }
+        }
+
+        #region The Indexer can be writtern returning bool
+        //public bool this[int index]
+        //{
+        //    get
+        //    {
+        //        if (index >= 0 && index <= 31)
+        //        {
+        //            if ((mValue & (1 << index)) == 0)
+        //                return false;
+        //            else
+        //                return true;
+        //        }
+        //        else
+        //        {
+        //            throw new ApplicationException(string.Format("Index {0} is invalid!", index));
+        //        }
+        //    }
+        //    set
+        //    {
+        //        if (index < 0 || index > 31)
+        //        {
+        //            throw new ApplicationException(string.Format("Index {0} is invalid.", index));
+        //        }
+        //        //Clear the bit at position index
+        //        mValue &= ~((uint)(1 << index));
+
+        //        //Set the bit at position index to value
+        //        mValue |= (uint)(value ? 1 : 0 << index);
+        //    }
+        //}
+        #endregion
+    }
+}
---
+++ 6IndexerBitArray/IndexerBitArray/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexerBitArray
+{
+    /// <summary>
+    /// A program showing how we might use a struct/class with indexer to help us using an array with 0s and 1s
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                BitArray32 arr = new BitArray32();
+                arr[0] = 1;
+                arr[5] = 1;
+                arr[5] = 0;
+                arr[25] = 1;
+                arr[31] = 1;
+                //arr[32] = 3;
+                //arr[7] = 3;
+                for (int i = 0; i <= 31; i++)
+                {
+                    Console.WriteLine("arr[{0}]={1}",i, arr[i]);
+                }
+            }
+            catch (ApplicationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
